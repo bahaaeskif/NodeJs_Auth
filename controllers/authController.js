af09042285bda357b483bd122eb0982a570fd2eb
Hexa <103849115+bahaaeskif@users.noreply.github.com>
2023-06-23 15:47:03 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,11 +47,11 @@ const createToken = (user) => {
 };
 // controller actions
 module.exports.signup_get = (req, res) => {
-  res.render('signup');
+  res.render('/api/v1/signup');
 }
 
 module.exports.login_get = (req, res) => {
-  res.render('login');
+  res.render('/api/v1/login');
 }
 
 module.exports.signup_post = async (req, res) => {
