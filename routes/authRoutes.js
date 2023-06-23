96593a35517b0ac1b62556aa34635939f97627c4
Hexa <103849115+bahaaeskif@users.noreply.github.com>
2023-06-23 15:44:03 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,9 +6,9 @@ const authController = require('../controllers/authController');
 
 const router = Router();
 
-router.get('api/v1/signup', authController.signup_get);
-router.post('api/v1/signup', authController.signup_post);
-router.get('api/v1/login', authController.login_get);
-router.post('api/v1/login', authController.login_post);
+router.get('/api/v1/signup', authController.signup_get);
+router.post('/api/v1/signup', authController.signup_post);
+router.get('/api/v1/login', authController.login_get);
+router.post('/api/v1/login', authController.login_post);
 
 module.exports = router;
